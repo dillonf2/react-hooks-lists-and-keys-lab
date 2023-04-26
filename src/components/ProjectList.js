ad3664fd119a87b99a8418839dfc728a5ea4a69e
Frank Dillon <dillonf2@outlook.com>
2023-04-26 13:22:47 -0400

    lists and keys lab completed

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,20 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
+
+
 function ProjectList({ projects }) {
   console.log(projects);
+
+  const itemsRendered= projects.map((obj)=>{
+    return (
+      <ProjectItem name={obj.name} about={obj.about} technologies={obj.technologies} key={obj.id}/>
+    )
+  })
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{itemsRendered}</div>
     </div>
   );
 }
